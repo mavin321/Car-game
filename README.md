942fa0b7b38caca956343d45b3caefd473b4e718
mavin321 <mavinpeteromondi@gmail.com>
2025-01-23 20:12:15 +0300

    version 2

--- README.md
+++ README.md
@@ -1,34 +1,39 @@
 🚗 Car Game
+
 Test your reflexes in this fast-paced car game! Use quick maneuvers to avoid oncoming cars and achieve the highest score.
 
 🎮 Features
+
 Simple controls: A to move left and D to move right.
 Endless gameplay with increasing difficulty.
 Score tracking to challenge yourself or compete with friends.
 Vibrant visuals and smooth mechanics.
+
 🛠️ Technologies Used
+
 Python
 Pygame
+
 🚀 How to Play
+
 Install Python
+
 Ensure you have Python 3.x installed on your system. Download it from python.org.
 
 Install Pygame
+
 Install the pygame library by running:
 
-bash
-Copy
-Edit
 pip install pygame  
-Run the Game
-Download or clone the game repository, navigate to the folder, and run:
 
-bash
-Copy
-Edit
-python car_game.py  
+Run the Game
+
+Download or clone the game repository, navigate to the folder, and run car_game.py
+
+ 
 Gameplay
 
+Left click the screen to start
 Use the A key to move your car left.
 Use the D key to move your car right.
 Avoid crashing into other cars to keep playing and increase your score.
--- car_game.py
+++ car_game.py
@@ -9,6 +9,10 @@ pygame.init()
 clock=pygame.time.Clock()
 fps=60
 
+#define font fpr score counter
+font=pygame.font.SysFont('Bauhaus 93', 60)
+white=(255, 255, 255)
+
 #create game wimdow
 screen_width=770
 screen_height=400
@@ -19,30 +23,47 @@ pygame.display.set_caption('road rush')
 scroll_speed=4
 score=0
 game_over=False
-driving=True
+driving=False
 car_frequency=3000
 last_car=pygame.time.get_ticks()-car_frequency
+pass_car=False
 
 #loading background pictures
-road1=pygame.image.load('road.png')
+road1=pygame.image.load('images/road.png')
 road1_rect = road1.get_rect()
 road1_rect.topleft = (0, 0)
-road2=pygame.image.load('road.png')
+road2=pygame.image.load('images/road.png')
 road2_rect = road2.get_rect()
 road2_rect.y = (-road1_rect.height)
+button_image= pygame.image.load('images/restart.png')
+
+#function to reset everything to restart the game
+def reset_game():
+    #remove all the NPC cars
+    npc_car_group.empty()
+    #reposition the bird
+    mycar.rect.x=270
+    mycar.rect.y=screen_height-160
+    
+
+
+#function to show score
+def draw_text(text,font,text_col,x ,y):
+    img=font.render(text, True, text_col)
+    screen.blit(img,(x,y))
 
 
 #create a class for the users car
 class Car(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self) # to inherit from the sprite function
-        self.image=pygame.image.load('maincar.png')
+        self.image=pygame.image.load('images/maincar.png')
         self.rect=self.image.get_rect()
         self.rect.center=[x,y]
         self.vel=3
 
     def update(self):
-        if driving==True:
+        if driving==True and game_over==False:
         #handle movement
             keys = pygame.key.get_pressed()
             if keys[pygame.K_w]:
@@ -62,7 +83,7 @@ class Car(pygame.sprite.Sprite):
 class NpcCar(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
-        self.image=pygame.image.load(f'cv{random.randint(1,4)}.png')
+        self.image=pygame.image.load(f'images/cv{random.randint(1,4)}.png')
         self.rect= self.image.get_rect()
         self.rect.center=[x,y]
         
@@ -73,15 +94,41 @@ class NpcCar(pygame.sprite.Sprite):
         if self.rect.top>screen_height:
             self.kill()
 
+#class for button
+class Button():
+    def __init__(self, x, y, image):
+        self.image=image
+        self.rect=self.image.get_rect()
+        self.rect.topleft=(x, y)
+
+    def draw(self):
+        action=False
+        #get mouse position
+        pos=pygame.mouse.get_pos()
+
+        #check if mouse is over the button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]==1: #shows mouse has been clicked
+                action=True
+
+        
+        #draw button
+        screen.blit(self.image,(self.rect.x, self.rect.y))
+
+        return action
+
 
 
 #create a group for users car and npc cars
-user_car=pygame.sprite.Group()
-npc_car=pygame.sprite.Group()
+user_car_group=pygame.sprite.Group()
+npc_car_group=pygame.sprite.Group()
 #now create an instance for the users car and place on screen 
 mycar=Car(270,screen_height-80)
 #add to group
-user_car.add(mycar)
+user_car_group.add(mycar)
+
+#create restart button instance
+button=Button(screen_width//2 - 50, screen_height//2 -100, button_image)
 
 
 #create game loop
@@ -97,16 +144,32 @@ while run:
     screen.blit(road2,road2_rect)
 
     #putting car on the screen
-    user_car.draw(screen)
-    user_car.update()
-    npc_car.draw(screen)
-
+    user_car_group.draw(screen)
+    user_car_group.update()
+    npc_car_group.draw(screen)
+    
+    #check if a user car has passed npc cars if so ad 1 to score
+    if len(npc_car_group)>0:
+        if user_car_group.sprites()[0].rect.top < npc_car_group.sprites()[0].rect.top and user_car_group.sprites()[0].rect.bottom < npc_car_group.sprites()[0].rect.bottom and pass_car==False:
+            pass_car=True
+        if pass_car== True:
+            if user_car_group.sprites()[0].rect.top > npc_car_group.sprites()[0].rect.top:
+                score+=1
+                pass_car=False
+    #display score
+    draw_text(str(score),font, white, int(screen_width/2),20)
+    
+    #check for collision
+    if pygame.sprite.groupcollide(user_car_group, npc_car_group,False, False):
+        game_over=True
+        
+    
     if game_over==False and driving== True:
         #generate new cars
         time_now=pygame.time.get_ticks()
         if time_now-last_car>car_frequency:
             obstacle_car=NpcCar(random.randint(150, 650),-screen_height)
-            npc_car.add(obstacle_car)
+            npc_car_group.add(obstacle_car)
             last_car=time_now
 
         #draw and scroll
@@ -116,11 +179,20 @@ while run:
             road1_rect.y=road2_rect.y-road1_rect.height
         if road2_rect.top >= screen_height:
             road2_rect.y=road1_rect.y-road1_rect.height
-        npc_car.update()
+        npc_car_group.update()
+    
+    if game_over==True:
+        if button.draw() == True:
+            game_over=False
+            reset_game()
+            score=0
+
     
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+        if event.type == pygame.MOUSEBUTTONDOWN and driving==False and game_over==False:
+            driving=True 
         
             
     
